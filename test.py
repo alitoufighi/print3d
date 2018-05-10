e29c03a86959afe21ad7abe318e6bd4629783918
shahabnikkhoo <shahabnikkhoo@gmail.com>
2018-05-10 19:23:44 +0000

    Update test.py

--- test.py
+++ test.py
@@ -188,5 +188,5 @@ def wifi():
 
 if __name__ == '__main__':
     print('let\'s go')
-    subprocess.Popen(["chromium-browser"," --noerrdialog","--no-sandbox","--kiosk", "--disable-translate", "--start-maximized", "http://0.0.0.0"])
+    subprocess.Popen(["chromium-browser","--overscroll-history-navigation=0","--disable-infobars"," --noerrdialog","--no-sandbox","--kiosk", "--disable-translate", "--start-maximized", "http://0.0.0.0"])
     app.run(host='0.0.0.0', port=80, threaded=True, debug=True, use_reloader=False)
