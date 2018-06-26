1dd71d8978227298d425a9b70e49ba96f3e600a7
SHB <shahabnikkhoo@gmail.com>
2018-06-26 05:10:59 -0700

    bug fix : release motor after stop print

--- utils.py
+++ utils.py
@@ -230,9 +230,11 @@ class Machine:
         for x in range(line_to_go, len(lines)):
             '''wait for buffer to be free'''
             while len(self.__Gcodes_to_run) >= 15:
-                ''' stop printing '''
+                ''' stop printing when it is wainting in buffer'''
                 if self.__stop_flag:
                     break
+
+
             signnum = lines[x].find(';')
             if not lines[x]:pass
             elif signnum == -1:
@@ -367,6 +369,10 @@ class Machine:
                 
                 self.__Gcodes_to_run = []
                 self.__Gcodes_return = []
+
+                #    release motors after stop  
+        		self.append_gcode(gcode="M84")
+
                 break
 
             ''' pause and resume the printing '''
